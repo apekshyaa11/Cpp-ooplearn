3471d9e5f368e48c5017cb170ff468d60d198b27
apekshyaa11 <apekshya.sapkota1125@gmail.com>
2025-08-23 21:14:38 +0545

    just demo code

---
+++ second.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int a,b,c,d,e,sum;
+    cout<<"Enter first number"<<endl;
+    cin>>a;
+    cout<<"Enter second number"<<endl;
+    cin>>b;
+    cout<<"Enter number"<<endl;
+    cin>>c;
+    cout<<"Enter number"<<endl;
+    cin>>d;
+    cout<<"Enter  number"<<endl;
+    cin>>e;
+    sum=a+b+c+d+e;
+    cout<<"Sum is: "<<sum<<endl;
+    return 0;
+}
