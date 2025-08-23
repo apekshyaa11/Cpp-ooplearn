5bbe368ce64a342595067ef1bed58604818024af
apekshyaa11 <apekshya.sapkota1125@gmail.com>
2025-08-23 14:20:35 +0545

    First Program

---
+++ firstprogram.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Be the princess of your own Fairy Tales"<<endl;
+    cout<<"Life is good when you think it is good "<<endl;
+    cout<<"Hello World, It's Apekshya here "<<endl;
+}
