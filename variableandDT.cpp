29981c21fe477c01e3d16c11a3de85a32052cd17
apekshyaa11 <apekshya.sapkota1125@gmail.com>
2025-08-29 16:17:13 +0545

    Variables and Data Types

---
+++ variableandDT.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+ #include<string>
+ using namespace std;
+ int main(){
+    int age=20;
+    float GPA=3.69;
+    double salary=4506.579;
+    char Grade='A';
+    string name="Alora";
+    bool isstudent=true;
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"GPA: "<<GPA<<endl;
+    cout<<"Salary: "<<salary<<endl;
+    cout<<"Grade: "<<Grade<<endl;
+    cout<<"is Student "<<isstudent<<endl;
+    return 0;
+ }
