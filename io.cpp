a344e3e7e3bcb6f96e2253bc2d583c9e17ae9167
apekshyaa11 <apekshya.sapkota1125@gmail.com>
2025-08-29 16:37:56 +0545

    Input/Output

---
+++ io.cpp
@@ -0,0 +1,13 @@
+//Input-Output
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your age ";
+    cin>>age;
+    cout<<"Hello! My name is "<<name<<", I am "<<age<<" years old."<<endl;
+    return 0;
+}
